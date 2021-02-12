5e2fe79cea0680d4d4c6c520c238e4106f37d12a
veronika <prokopchuk_veron@mail.ru>
2021-02-12 20:38:54 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,122 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<button className="button-usual button-color-black">*/}
+      {/*    USUAL*/}
+      {/*</button>*/}
+      {/*  <button className="button-usual button-color-gold">*/}
+      {/*    USUAL*/}
+      {/*</button><button className="button-usual button-color-black">*/}
+      {/*    USUAL*/}
+      {/*</button><button className="button-usual button-color-gold">*/}
+      {/*    USUAL*/}
+      {/*</button><button className="button-usual button-color-gold">*/}
+      {/*    USUAL*/}
+      {/*</button><button className="button-usual">*/}
+      {/*    USUAL*/}
+      {/*</button>*/}
+
+        <div className="calendar-rectangle-grey button-color-grey">
+            <div className="calendar-rectangle-white button-color-white">
+                <div className="button-oval">
+                </div>
+                <div className="button-circle">
+                </div>
+                <div className="calendar-rectangle-white button-color-white">
+                    <div className="f-s-14 pudding20x15x15x15">
+                        Edit Access to Google Analytics
+                    </div>
+                    <div className="f-s-18">
+                        January 2019
+                    </div>
+
+                    <div className="week-header">
+                        <span>M</span>
+                        <span>t</span>
+                        <span>w</span>
+                        <span>t</span>
+                        <span>f</span>
+                        <span>s</span>
+                        <span>s</span>
+                    </div>
+
+                    <div className="days-header">
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
+                        <span>1</span>
+                    </div>
+                    <div className="days-header">
+                        <span>2</span>
+                        <span>3</span>
+                        <span>5</span>
+                        <span>6</span>
+                        <span>7</span>
+                        <span>8</span>
+                        <span>9</span>
+                    </div>
+                    <div className="days-header">
+                        <span>10</span>
+                        <span>11</span>
+                        <span>12</span>
+                        <span>13</span>
+                        <span>14</span>
+                        <span>15</span>
+                        <span>16</span>
+                    </div>
+                    <div className="days-header">
+                        <span>17</span>
+                        <span>18</span>
+                        <span>19</span>
+                        <span>20</span>
+                        <span>21</span>
+                        <span>22</span>
+                        <span>23</span>
+                    </div>
+                    <div className="days-header">
+                        <span>24</span>
+                        <span>25</span>
+                        <span>26</span>
+                        <span>27</span>
+                        <span>28</span>
+                        <span>29</span>
+                        <span>30</span>
+                    </div>
+                    <div className="days-header">
+                        <span>31</span>
+                    </div>
+                    <div className="calendar-line-grey button-color-grey">
+                    </div>
+                    <div className="calendar-rectangle-324x180 button-color-white">
+                        <div className="f-s-14 pudding5x5x5x5">
+                            Goals / KPI
+                        </div>
+
+                        <div className="calendar-rectangle-324x161 button-color-white ">
+                            <div className="f-s-14 pudding5x5x5x5 write-color-black">
+                                You can add as many users as you want to view, comment, export, and access redlines.
+                            </div>
+                            <div className="f-s-14 pudding5x5x5x5 write-color-black">
+                                Unlimited projects. Unlimited version history.
+                            </div>
+                            <div className="f-s-14 pudding5x5x5x5 write-color-black">
+                                A team library with unlimited shared You can add as many users as you want to view,
+                                comment, export, and access redlines.
+                            </div>
+                        </div>
+
+                        <div className="f-s-14 write-color-gold">
+                            Edit goals / KPI
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
